d255803cbdb120370fee95817e58be28cc32560b
FrancescoCina <francesco.cina04@gmail.com>
2021-07-28 00:28:41 +0200

    Inserita funzione attraverso methods

--- js/script.js
+++ js/script.js
@@ -13,5 +13,16 @@ const root = new Vue({
         favoriteFramework: "",
         imgLink: "./img/Logo_Vue.png",
         imgDescription: "Logo Vue.js",
+    },
+    methods: {
+        changeImg() {
+            if (this.favoriteFramework === "vue" || this.favoriteFramework === "Vue") {
+                this.imgLink = "./img/Logo_Vue.png"
+                this.imgDescription = "Logo Vue.js"
+            } else {
+                this.imgLink = " "
+                this.imgDescription = "Hai scelto il framework sbagliato!"
+            }
+        }
     }
 })
