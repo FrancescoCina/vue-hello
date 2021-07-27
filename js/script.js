a52c2261102cb9f8ab1b9ebcd9e753dc2ac039ff
FrancescoCina <francesco.cina04@gmail.com>
2021-07-27 18:31:09 +0200

    Stampato h1 in HTML prendendo dati dall'oggetto "data:" e agginunte le classi di riferimento

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: "#root",
     data: {
-
+        name: "Francesco",
+        surname: "Cina",
+        fontSizeClass: ".fs-5",
+        colorClass: "red",
     }
 })
